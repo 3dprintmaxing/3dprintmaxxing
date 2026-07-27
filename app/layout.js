f0c6b2325d83bea3211ec4aeda8d1506e4cdfb3b
zocomputer <help@zocomputer.com>
2026-07-27 03:29:59 +0000

    Use catalog metadata in localized route layout

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,3 @@
-import { generatePageMetadata } from './metadata';
-
-export async function generateMetadata({ params }) {
-  return generatePageMetadata({ params });
-}
-
 export default function RootLayout({ children }) {
   return <html lang="en"><body>{children}</body></html>;
 }
