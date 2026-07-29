87de79c4d3f644b2571d1679b0ad1afc653e6724
zocomputer <help@zocomputer.com>
2026-07-29 16:37:01 +0000

    Add Google Analytics to every page

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="/styles.min.css" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JT6T953WEC"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-JT6T953WEC');
+        ` }} />
       </head>
       <body>
         {children}
